d7b404512ce6c8e1b60d9535c7cf726907923e16
mariusrundereim <marius.rundereim@gmail.com>
2023-10-24 10:07:52 +0200

    remove improt

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 require("dotenv/config"); // filepath
-import "./node_modules/dotenv/config"; // husk type modules
+//import "./node_modules/dotenv/config"; // husk type modules
 
 console.log(process.env.API_KEY);
