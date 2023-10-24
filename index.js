a97abd0080cacac8928a399bece29e6fff96822f
mariusrundereim <marius.rundereim@gmail.com>
2023-10-24 10:30:18 +0200

    update secret in yml

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ require("dotenv/config"); // filepath
 //import "./node_modules/dotenv/config"; // husk type modules
 
 console.log(process.env.API_KEY);
+console.log(`This is the key: ${process.env.API_KEY}`);
+
+console.log(Number(process.env.SECRET) === 88 ? true : false);
