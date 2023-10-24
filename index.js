34df09a64d06bbdd1679b27d5e612e752bc0f9fb
mariusrundereim <marius.rundereim@gmail.com>
2023-10-24 10:56:59 +0200

    test

--- index.js
+++ index.js
@@ -5,10 +5,3 @@ console.log(process.env.API_KEY);
 console.log(`This is the key: ${process.env.API_KEY}`);
 
 console.log(Number(process.env.SECRET) === 88 ? true : false);
-
-const apiNumber = Number(process.env.SECRET);
-
-const secretNum = document.querySelector("#secretNum");
-
-secretNum.textContent = apiNumber;
-console.log(secretNum);
